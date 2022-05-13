d4097cf822d15dfa4b574a959c199ec5652925d6
Hector Bustillos <hector.bustillos.97@gmail.com>
2022-05-12 22:16:56 -0600

    Life at its finest

--- grid.rb
+++ grid.rb
@@ -1,2 +1,57 @@
+require_relative 'cell'
+
 class Grid
+  attr_accessor :matrix
+  attr_reader :width, :height
+
+  def initialize(width = 6, height = 6)
+    @width = width
+    @height = height
+
+    @matrix = build_matrix
+  end
+
+  def build_matrix
+    Array.new(width).map.with_index do |x, i|
+      Array.new(height).map.with_index do |y, j|
+        Cell.new(i, j)
+      end
+    end
+  end
+
+  def living_cells
+    @matrix.flatten.select{ |cell| cell.alive? }
+  end
+
+  def dead_cells
+    @matrix.flatten.select{ |cell| cell.dead? }
+  end
+
+  def alive_neighbours_for(cell)
+    neighbours_for(cell).count(&:alive?)
+  end
+
+  def neighbours_for(cell)
+    x, y = cell.position
+
+    left = [
+      [x - 1, y - 1].any?(&:negative?) ? nil : @matrix[x - 1]&.at(y - 1),
+      [x - 1, y + 1].any?(&:negative?) ? nil : @matrix[x - 1]&.at(y + 1),
+      (x - 1).negative? ? nil : @matrix[x - 1]&.at(y)
+    ]
+
+    middle = [
+      (y + 1).negative? ? nil : @matrix[x]&.at(y + 1),
+      (y - 1).negative? ? nil : @matrix[x]&.at(y - 1)
+    ]
+
+    right = [
+      [x + 1, y - 1].any?(&:negative?) ? nil : @matrix[x + 1]&.at(y - 1),
+      [x + 1, y + 1].any?(&:negative?) ? nil : @matrix[x + 1]&.at(y + 1),
+      (x + 1).negative? ? nil : @matrix[x + 1]&.at(y)
+    ]
+
+    (left + middle + right).compact
+  end
+
 end
--- life.rb
+++ life.rb
@@ -7,5 +7,40 @@
 
 # https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life
 
+require_relative 'god'
+require_relative 'grid'
+
 class Life
+  attr_reader :grid, :cycle_count, :time_interval
+
+  def initialize(width, height, time_interval = 1000)
+    @width = width
+    @height = height
+    @time_interval = time_interval
+
+    @grid = Grid.new(width, height)
+    @cycle_count = 1
+  end
+
+  def start
+    loop do
+      total_cells_alive = grid.living_cells.count
+      total_cells_dead = grid.dead_cells.count
+
+      # TODO: Create UI to visualy display life cycle and replace text prints.
+      p "Cycle count: #{cycle_count}"
+      p "Total Cells alive: #{total_cells_alive}"
+      p "Total Cells dead: #{total_cells_dead}"
+      p "----------------------------------------"
+
+      life_cycle = God.life_cycle(grid)
+      @cycle_count += 1
+
+      God.kill(life_cycle[:to_kill])
+      God.give_birth(life_cycle[:to_give_birth])
+
+      sleep time_interval/1000
+    end
+  end
+
 end
